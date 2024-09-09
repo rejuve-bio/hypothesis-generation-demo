a48e620eec559944e54aaf0e9bf04889bd970dfa
wendecoder <wenonezra258@gmail.com>
2024-09-09 04:01:16 -0700

    Fix bugs in the check_enrich and check_hypothesis functions

--- db.py
+++ db.py
@@ -40,13 +40,13 @@ class Database:
         if user_id:
             query['user_id'] = user_id
         if hypothesis_id:
-            query['hypothesis_id'] = hypothesis_id
+            query['id'] = hypothesis_id
 
         hypotheses = list(self.hypothesis_collection.find(query))
         for hypothesis in hypotheses:
             hypothesis['_id'] = str(hypothesis['_id'])
 
-        return hypotheses if len(hypotheses) > 1 else (hypotheses[0] if hypotheses else None)
+        return hypotheses if hypotheses else []
 
     def check_hypothesis(self, user_id=None, enrich_id=None, go_id=None):
         query = {}
@@ -70,17 +70,20 @@ class Database:
         if phenotype:
             query['phenotype'] = phenotype
         if variant_id:
-            query['variant_id'] = variant_id
+            query['variant'] = variant_id
+
+        print("this is the query: ", query)
         
         enrich = self.enrich_collection.find_one(query)
         
         return enrich is not None
 
 
-    def get_hypothesis_by_enrich_and_go(self, enrich_id, go_id):
+    def get_hypothesis_by_enrich_and_go(self, enrich_id, go_id, user_id=None):
         query = {
             'enrich_id': enrich_id,
-            'go_id': go_id
+            'go_id': go_id,
+            'user_id': user_id
         }
         hypothesis = self.hypothesis_collection.find_one(query)
         if hypothesis:
@@ -88,10 +91,11 @@ class Database:
 
         return hypothesis
 
-    def get_enrich_by_phenotype_and_variant(self, phenotype, variant_id):
+    def get_enrich_by_phenotype_and_variant(self, phenotype, variant_id, user_id=None):
         query = {
             'phenotype': phenotype,
-            'variant_id': variant_id
+            'variant': variant_id,
+            'user_id': user_id
         }
         
         enrich = self.enrich_collection.find_one(query)
@@ -108,24 +112,25 @@ class Database:
         if user_id:
             query['user_id'] = user_id
         if enrich_id:
-            query['enrich_id'] = enrich_id
+            query['id'] = enrich_id
 
+        print("this is query: ", query)
         enriches = list(self.enrich_collection.find(query))
         print("these are enriches: ", enriches)
-        enrich = {}
+        # enrich = {}
         for enrich in enriches:
             enrich['_id'] = str(enrich['_id'])
 
-        return enrich if len(enrich) > 1 else (enrich[0] if enrich else None)
+        return enriches if enriches else []
 
     def delete_hypothesis(self, user_id, hypothesis_id):
-        result = self.hypothesis_collection.delete_one({'hypothesis_id': hypothesis_id, 'user_id': user_id})
+        result = self.hypothesis_collection.delete_one({'id': hypothesis_id, 'user_id': user_id})
         if result.deleted_count > 0:
             return {'message': 'Hypothesis deleted'}, 200
         return {'message': 'Hypothesis not found or not authorized'}, 404
     
     def delete_enrich(self, user_id, enrich_id):
-        result = self.enrich_collection.delete_one({'enrich_id': enrich_id, 'user_id': user_id})
+        result = self.enrich_collection.delete_one({'id': enrich_id, 'user_id': user_id})
         if result.deleted_count > 0:
             return {'message': 'Enrich deleted'}, 200
         return {'message': 'Enrich not found or not authorized'}, 404
--- pl/queries.pl
+++ pl/queries.pl
@@ -4,11 +4,10 @@
 :- use_module(library(interpolate)).
 
 candidate_genes(S, Genes) :-
-    setof(Gene, in_tad_with(S, Gene), Gs),
-    %TODO optimize with_k_distance
+    setof(Gene, in_tad_with(S, Gene), InTadGenes),
     % setof(Gene, within_k_distance(Gene, S, 500000), ClosestGenes),
     % union(InTadGenes, ClosestGenes, Gs),
-    maplist(gene_name, Gs, Genes).
+    maplist(gene_name, InTadGenes, Genes).
 
 gene_id(Name, Id) :- gene_name(gene(Id), Name).
 
@@ -22,19 +21,45 @@ variant_id(S, Id) :-
     upcase_atom(A, Alt),
     Id = "$Chr:$Start-$End-$Ref>$Alt".
 
+% within_k_distance(G, S, K) :-
+%     G = gene(_), % limit the search to Genes
+%     chr(G, Chr),
+%     chr(S, Chr),
+%     (start(G, StartG),
+%         start(S, StartS),
+%         abs(StartS - StartG) =< K)
+%     ;(end(G, EndG),
+%         end(S, EndS),
+%         abs(EndS - EndG) =< K).
+
 within_k_distance(G, S, K) :-
-    chr(G, Chr),
+    % G = gene(_), % limit the search to Genes
     chr(S, Chr),
+    start(S, Pos),
+    chr(G, Chr),
     start(G, StartG),
     end(G, EndG),
-    start(S, StartS),
-    end(S, EndS),
-    StartDist is abs(StartS - StartG),
-    EndDist is abs(EndS - EndG), 
-    (StartDist =< K
-    ; EndDist =< K).
+    (abs(Pos - StartG) =< K, !
+    ; abs(Pos - EndG) =< K).
+
+
+% Get the TF with lowest loss score for a SNP.
+find_and_rank_tfs(SNP, T, G) :-
+    setof(Score-TF, (regulates(TF, G), tfbs_snp(TF, SNP), effect(tfbs_snp(TF, SNP), loss), score(tfbs_snp(TF, SNP), Score)), TFScorePairs),
+    keysort(TFScorePairs, RankedPairs),
+    pairs_values(RankedPairs, RankedTFs),
+    take(1, RankedTFs, [T]).
+
+
+take(0, _, []).
+take(_, [], []).
+take(N, [H|T], [H|Rest]) :-
+    N > 0,
+    N1 is N - 1,
+    take(N1, T, Rest).
 
 server_start(Port) :- http_server(http_dispatch, [port(Port)]).
+server_stop(Port) :- http_stop_server(Port, []).
 
 :- use_module(library(sandbox)).
 :- multifile sandbox:safe_primitive/1.
