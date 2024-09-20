afe4a7874a8b2c51117404197f6f15cf8a382db0
wendecoder <wenonezra258@gmail.com>
2024-09-20 05:29:35 -0400

    - Reverted deleted comments inside prolog files.

--- db.py
+++ db.py
@@ -52,7 +52,7 @@ class Database:
         for hypothesis in hypotheses:
             hypothesis['_id'] = str(hypothesis['_id'])
 
-        return hypotheses if len(hypotheses) > 1 else (hypotheses[0] if hypotheses else None)
+        return hypotheses if hypotheses else []
 
     def check_hypothesis(self, user_id=None, enrich_id=None, go_id=None):
         query = {}
@@ -83,10 +83,11 @@ class Database:
         return enrich is not None
 
 
-    def get_hypothesis_by_enrich_and_go(self, enrich_id, go_id):
+    def get_hypothesis_by_enrich_and_go(self, enrich_id, go_id, user_id=None):
         query = {
             'enrich_id': enrich_id,
-            'go_id': go_id
+            'go_id': go_id,
+            'user_id': user_id
         }
         hypothesis = self.hypothesis_collection.find_one(query)
         if hypothesis:
@@ -94,10 +95,11 @@ class Database:
 
         return hypothesis
 
-    def get_enrich_by_phenotype_and_variant(self, phenotype, variant_id):
+    def get_enrich_by_phenotype_and_variant(self, phenotype, variant_id, user_id=None):
         query = {
             'phenotype': phenotype,
-            'variant_id': variant_id
+            'variant': variant_id,
+            'user_id': user_id
         }
         
         enrich = self.enrich_collection.find_one(query)
@@ -130,14 +132,13 @@ class Database:
         return enriches if enriches else []
     
     def delete_hypothesis(self, user_id, hypothesis_id):
-        result = self.hypothesis_collection.delete_one({'hypothesis_id': hypothesis_id, 'user_id': user_id})
+        result = self.hypothesis_collection.delete_one({'id': hypothesis_id, 'user_id': user_id})
         if result.deleted_count > 0:
             return {'message': 'Hypothesis deleted'}, 200
         return {'message': 'Hypothesis not found or not authorized'}, 404
     
     def delete_enrich(self, user_id, enrich_id):
-        result = self.enrich_collection.delete_one({'enrich_id': enrich_id, 'user_id': user_id})
+        result = self.enrich_collection.delete_one({'id': enrich_id, 'user_id': user_id})
         if result.deleted_count > 0:
             return {'message': 'Enrich deleted'}, 200
         return {'message': 'Enrich not found or not authorized'}, 404
-    
