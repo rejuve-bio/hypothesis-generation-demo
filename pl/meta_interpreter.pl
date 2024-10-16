e112dc9643e4b3de470c4dbf86daef492116c0d2
wendecoder <wenonezra258@gmail.com>
2024-10-16 17:24:58 +0300

    Fix mongo and prefect service issue

--- pl/meta_interpreter.pl
+++ pl/meta_interpreter.pl
@@ -1,6 +1,5 @@
 :- module(meta_intepreter, [
-    proof_tree/2,
-    proof_tree/2,
+    prooftree/2,
     json_proof_tree/2
   ]).
 
@@ -49,6 +48,15 @@ export_proof(Out, t(or, C, SubProofs)) :- !,
     ; (GProofs = [P], 
       export_proof(Out, P))).
 
+% export_proof(Out, t(or, C, SubProofs)) :- !,
+%   ((length(SubProofs, L), L > 1)
+%   -> (dot_node(Out, C, [label(or)]), 
+%      maplist(export_subproof(Out, C), SubProofs))
+%   ; (
+%     SubProofs = [P],
+%     export_proof(Out, P)
+%   )).
+
 export_proof(Out, Proof) :-
   Proof = t(Rule,Concl,SubProofs),
   dot_node(Out, Concl),
@@ -71,22 +79,11 @@ export_subproof(Out, Proof, SubProof) :-
 
 mi(true, t(true, true, [])) :- !.
 
-mi((hideme(A), B), PB) :- !,
-mi((hideme(A), B), PB) :- !,
+mi((hideme(A), B), PB) :- 
   call(A), mi(B, PB).
 
-mi(hideme((A, B)), t(hideme, hideme, [])) :- !,
-  A, B.
-
-mi((A, hideme(B)), PA) :- !,
-mi(hideme((A, B)), t(hideme, hideme, [])) :- !,
-  A, B.
-
-mi((A, hideme(B)), PA) :- !,
+mi((A, hideme(B)), PA) :- 
   mi(A, PA), call(B).
-
-mi(hideme(X), t(hideme, hideme, [])) :- !,
-   call(X).
 
 mi((A, B), t(and, C, [PA, PB])) :- !, %conjuction
     copy_term(and(A, B), C),
@@ -105,15 +102,12 @@ mi((A, B), t(and, C, [PA, PB])) :- !, %conjuction
 %     mi(B, PB).
 
 
-mi((A;B), t(or, _, Proof)) :- !,
+mi((A;B), Proof) :- !,
     findall(ProofA, mi(A, ProofA), ProofAs),
     findall(ProofB, mi(B, ProofB), ProofBs),
-    (ProofAs = [ProofA], ProofBs = [ProofB] -> Proof = [ProofA, ProofB] ;
-     ProofAs = [ProofA] -> Proof = [ProofA] ;
-     ProofBs = [ProofB] -> Proof = [ProofB]).
-    (ProofAs = [ProofA], ProofBs = [ProofB] -> Proof = [ProofA, ProofB] ;
-     ProofAs = [ProofA] -> Proof = [ProofA] ;
-     ProofBs = [ProofB] -> Proof = [ProofB]).
+    (ProofAs = [ProofA], ProofBs = [ProofB] -> Proof = t(or, _, [ProofA, ProofB]) ;
+     ProofAs = [ProofA] -> Proof = ProofA ;
+     ProofBs = [ProofB] -> Proof = ProofB).
 
 mi(findall(X, G, Ls), t(R, C, SP)) :- !,
     findall(t(G, X, [P]), mi(G, P), Xs),
@@ -132,12 +126,19 @@ mi(subset(X, Xs), t(R, C, [])) :- !,
   C = "subset_of({$X1, $X2,...}, {$Xs1, $Xs2, $Xs3...})",
   R = "{$X1, $X2,...}".
 
+mi(hideme(X), t(hideme, hideme, [])) :- !,
+   call(X).
+
+
 mi(G, t(built_in, G, [])) :- % Check if the goal is a built-in predicate.
     G \= true,
     G \= findall(_, _, _),
     G \= hideme(_),
+    % G \= relevant_gene_coexpression(_, _),
     (predicate_property(G, built_in) ; %or
     \+ predicate_property(G,number_of_clauses(_))), !,
+    % functor(G, C, _),
+    % format("Built in Gs: ~w \n", [G]),
     call(G). % Directly call the built-in predicate.
 
 
@@ -148,17 +149,9 @@ mi(G, t(R, G, [P])) :-
     G \= hideme(_),
     clause(G, Body, Ref), 
     clause(HeadC, BodyC, Ref),
-    % without_hidden(BodyC, BodyF),
-    % without_hidden(BodyC, BodyF),
+    without_hidden(BodyC, BodyF),
     copy_term(HeadC :- BodyF, R), 
     mi(Body, P).
-
-rule_body(R, RB) :-
-  clause(relevant_gene(G, S), Body, Ref), 
-  clause(HeadC, BodyC, Ref), 
-  copy_term(HeadC :- BodyC, Term), 
-  numbervars(Term),
-  RB = "$Term".
 
 without_hidden((hideme(_), hideme(_)), true) :- !.
 without_hidden((X, hideme(_)), WX) :-
@@ -211,79 +204,75 @@ without_hidden(X, X) :-
 %   ;explanation_of_(S2, E)).
 
 % Predicate to convert an 'and' compound clause to a list of terms
-%and_to_list(and(Term1, Term2), List) :-
-%    and_to_list(Term1, List1),
-%    and_to_list(Term2, List2),
-%    append(List1, List2, List).
-%and_to_list(Term, [Term]).
+and_to_list(and(Term1, Term2), List) :-
+    and_to_list(Term1, List1),
+    and_to_list(Term2, List2),
+    append(List1, List2, List).
+and_to_list(Term, [Term]).
 
 
 % Predicate to extract terms that do not contain variables or 'hideme(_)'
 extract_ground_terms(Term, List) :-
-    extract_terms(Term, List).
-    %extract_terms(Term, AllTerms),
-    %exclude(contains_var_or_hideme, AllTerms, FilteredTerms),
-    %exclude(is_true_term, FilteredTerms, List).
+    extract_terms(Term, AllTerms),
+    exclude(contains_var_or_hideme, AllTerms, FilteredTerms),
+    exclude(is_true_term, FilteredTerms, List).
 
 % Recursive helper predicate to extract all terms
 extract_terms(true, []) :- !.
-%extract_terms(hideme(_), []) :- !.
+extract_terms(hideme(_), []) :- !.
 extract_terms(t(true, true, []), []) :- !.
-extract_terms(t(hideme, hideme, []), []) :- !.
-extract_terms(t(hideme, hideme, []), []) :- !.
 extract_terms(t(built_in, G, []), [G]) :- !.
 extract_terms(t(and, _, SubProof), Terms) :- !,
     maplist(extract_terms, SubProof, SubTerms),
     flatten(SubTerms, Terms).
 
-extract_terms(t(or, _, SubProof), Terms) :- !,
+extract_terms(t(or_left, _, SubProof), Terms) :- !,
     maplist(extract_terms, SubProof, SubTerms),
     flatten(SubTerms, Terms).
 
-%extract_terms(t(or_right, _, SubProof), Terms) :- !,
-%    maplist(extract_terms, SubProof, SubTerms),
-%    flatten(SubTerms, Terms).
+extract_terms(t(or_right, _, SubProof), Terms) :- !,
+    maplist(extract_terms, SubProof, SubTerms),
+    flatten(SubTerms, Terms).
     
 extract_terms(t(_, C, SubProof), Terms) :- !,
     extract_terms(C, Term),
     maplist(extract_terms, SubProof, SubTerms),
-    flatten([Term|SubTerms], Terms),
-    %append(Term, FlatSubTerms, Terms).
+    flatten(SubTerms, FlatSubTerms),
+    append(Term, FlatSubTerms, Terms).
     
 extract_terms(Term, [Term]) :- !.
 
 % Helper predicate to check if a term contains variables or 'hideme(_)'
-%contains_var_or_hideme(Term) :-
-%    term_variables(Term, Vars),
-%    Vars \= [],
-%    !.
-%contains_var_or_hideme(Term) :-
-%    compound(Term),
-%    Term =.. [hideme|_],
-%    !.
-%contains_var_or_hideme(Term) :-
-%    compound(Term),
-%    Term =.. [_|Args],
-%    maplist(contains_var_or_hideme, Args).
+contains_var_or_hideme(Term) :-
+    term_variables(Term, Vars),
+    Vars \= [],
+    !.
+contains_var_or_hideme(Term) :-
+    compound(Term),
+    Term =.. [hideme|_],
+    !.
+contains_var_or_hideme(Term) :-
+    compound(Term),
+    Term =.. [_|Args],
+    maplist(contains_var_or_hideme, Args).
 
 % Helper predicate to check if a term is 't(true, true, [])'
-%is_true_term(t(true, true, [])).
+is_true_term(t(true, true, [])).
 
 % Flatten a list of lists
 % Predicate to flatten a list of lists
-%flatten([], []).
-%flatten([L|Ls], Flat) :-
-%    flatten(L, NewL),
-%    flatten(Ls, NewLs),
-%    append(NewL, NewLs, Flat).
-%flatten(L, [L]).
+flatten([], []).
+flatten([L|Ls], Flat) :-
+    flatten(L, NewL),
+    flatten(Ls, NewLs),
+    append(NewL, NewLs, Flat).
+flatten(L, [L]).
 
 
 % Predicate to extract nodes and edges
 extract_nodes_edges([], [], []).
 extract_nodes_edges([Term|Terms], Nodes, Edges) :-
-    (Term =.. [Relation, Subject, Object]
-    ; Term =.. [Relation, Subject, Object, _]), %Todo handle edge properties
+    Term =.. [Relation, Subject, Object],
     extract_nodes_edges(Terms, NodesTail, EdgesTail),
     sort([Subject, Object | NodesTail], Nodes), % Remove duplicates
     Edges = [[label(Relation), source(Subject), target(Object)] | EdgesTail].
@@ -291,8 +280,8 @@ extract_nodes_edges([Term|Terms], Nodes, Edges) :-
 node_to_json(Node, json([id=NodeId, type=Type])) :-
   Node =.. [Type, NodeId].
 
-edge_to_json([label(Relation), source(Subject), target(Object)], json([source=Source, target=Target, label=Relation])) :-
-edge_to_json([label(Relation), source(Subject), target(Object)], json([source=Source, target=Target, label=Relation])) :-
+edge_to_json([label(Relation), source(Subject), target(Object)], json([source=Source, target=Target, 
+                                label=Relation])) :-
   Subject =.. [_, Source],
   Object =.. [_, Target].
 
@@ -303,8 +292,7 @@ create_json_graph([_|Terms], JSONGraph) :-
     maplist(edge_to_json, EdgesList, EdgesJSON),
     JSONGraph = json([nodes=NodesJSON, edges=EdgesJSON]).
 
-proof_tree(A, PT):-
-proof_tree(A, PT):-
+prooftree(A, PT):-
   mi(A, PT),
   numbervars(PT).
 
@@ -316,19 +304,19 @@ proof_tree(A, PT):-
 %   read_file_to_string(File, PT, []).
 
 json_proof_tree(A, Graph) :-
-  proof_tree(A, Proof),
-  proof_tree(A, Proof),
+  prooftree(A, Proof),
   extract_ground_terms(Proof, Terms),
   create_json_graph(Terms, JsonGraph),
   atom_json_term(Graph, JsonGraph, []).
+  % tmp_file_stream(text, File, Out), 
+  % json_write(Out, JsonGraph), 
+  % close(Out),
+  % read_file_to_string(File, Graph, []).
 
 % :- use_module(pengine_sandbox:library(meta_intepreter)).
 :- use_module(library(sandbox)).
 
 :- multifile sandbox:safe_primitive/1.
 
-sandbox:safe_primitive(meta_intepreter:proof_tree(_,_)).
-sandbox:safe_primitive(meta_intepreter:proof_tree(_,_)).
+sandbox:safe_primitive(meta_intepreter:prooftree(_,_)).
 sandbox:safe_primitive(meta_intepreter:json_proof_tree(_,_)).
-sandbox:safe_primitive(meta_intepreter:rule_body(_,_)).
-
