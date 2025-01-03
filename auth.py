953e844f483148f2f378c4a4c0960010f24a6822
Tesnim19 <tesnimabdi67@gmail.com>
2025-01-03 02:42:25 -0800

    Add token retrieval to token_required decorator in auth.py

--- auth.py
+++ auth.py
@@ -1,4 +1,4 @@
-from flask_jwt_extended import jwt_required, get_jwt_identity
+from flask_jwt_extended import jwt_required, get_jwt_identity, get_jwt
 from functools import wraps
 from flask import jsonify
 
@@ -7,10 +7,11 @@ def token_required(f):
     @jwt_required()  # This ensures the token is valid
     def decorated(self, *args, **kwargs):
         current_user_id = get_jwt_identity()  # Fetch the user identity from the token
+        token = get_jwt()
         if not current_user_id:
             return jsonify({'message': 'Token is invalid!'}), 403
 
         # Pass current_user_id to the decorated function
-        return f(self, current_user_id, *args, **kwargs)
+        return f(self, current_user_id, token, *args, **kwargs)
     
     return decorated
