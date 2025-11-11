4c0885cc079a2aeb0001bf4ad9c8653e97666282
Tesnim19 <tesnimabdi67@gmail.com>
2025-11-11 12:00:00 -0800

    feat(api): add PhenotypesAPI for managing phenotype data and bulk loading

--- config.py
+++ config.py
@@ -5,7 +5,8 @@ from query_swipl import PrologQuery
 from db import (
     UserHandler, ProjectHandler, FileHandler, AnalysisHandler,
     EnrichmentHandler, HypothesisHandler, SummaryHandler, TaskHandler,
-    GeneExpressionHandler
+    GeneExpressionHandler,
+    PhenotypeHandler
 )
 
 class Config:
@@ -141,5 +142,6 @@ def create_dependencies(config):
         'hypotheses': HypothesisHandler(mongodb_uri, db_name),
         'summaries': SummaryHandler(mongodb_uri, db_name),
         'tasks': TaskHandler(mongodb_uri, db_name),
-        'gene_expression': GeneExpressionHandler(mongodb_uri, db_name)
+        'gene_expression': GeneExpressionHandler(mongodb_uri, db_name),
+        'phenotypes': PhenotypeHandler(mongodb_uri, db_name)
     }
